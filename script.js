d5b42b50c5a4f441edfb256f5c463704a9d70497
TajRask <roqayah1994@gmail.com>
2022-12-22 22:46:57 +0300

    js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const randomResp = await fetch ("https://www.themealdb.com/api/json/v1/1/random.
 const randomMeal = await randomResp.json();
 const randomResult=randomMeal.meals[0]
 
-console.log(randomResult);
+addMeal(randomResult,true);
 
 }
 
@@ -20,3 +20,20 @@ async function fetchMealBySearch(term){
     const searchMeal =await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"+ term)
 }
 
+function addMeal(mealData, random=false){
+    const meal= document.createElement('div');
+    meal.classList.add('meal');
+    meal.innerHTML=`  
+    <div class="meal-head">
+        ${random ?`<sapn class="random">وصفة اليوم</sapn>`: ''}
+
+        <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}" height="350" width="350">
+    </div>
+
+    <div class="meal-body">
+        <h3>${mealData.strMeal}/h3>
+        <button class="like-btn active" >إعجاب</button>
+    </div> 
+    `
+
+}
