2ef6afd2d28912eeafb05117bbc9e0b11cd85e7f
TajRask <roqayah1994@gmail.com>
2022-12-22 20:41:19 +0300

    API

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+fetchRandomMeal()
+
+async function fetchRandomMeal(){
+const randomResp = await fetch ("https://www.themealdb.com/api/json/v1/1/random.php");
+const randomMeal = await randomResp.json();
+const randomResult=randomMeal.meals[0]
+
+console.log(randomResult);
+
+}
+
+
+async function fetchMealById(id){
+    const idMeal=await fetch("www.themealdb.com/api/json/v1/1/lookup.php?i=52772 "+id)
+}
+
+async function fetchMealBySearch(term){
+    const searchMeal =await fetch("www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"+term)
+}
+
